157077432e5129de7817750d243b8ec3e1c989e1
bahmanapl <apl.bahman@gmail.com>
2023-11-06 23:22:17 -0800

    JS Practice

---
+++ practice-one.js
@@ -0,0 +1,23 @@
+function sayBahmanName() {
+    console.log("hello bahman")
+  }
+
+  let exampleObject = {
+    id: 1,
+    name: "Bahman"
+}
+
+
+console.log(exampleObject);
+console.log("My name is: ",exampleObject.name);
+
+
+function doSomething(){
+    let functionVar = 10;
+    for(let i =0; i <10; i++){
+        console.log(i);
+    }
+}
+
+
+doSomething();
